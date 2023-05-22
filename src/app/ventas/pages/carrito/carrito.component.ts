c18cef19d374e636380d642d5f9b221e62c950d1
henryalejandro1229 <101129308+henryalejandro1229@users.noreply.github.com>
2023-05-21 23:29:46 -0600

    Feat - estructura inicial componente carrito

--- src/app/ventas/pages/carrito/carrito.component.ts
+++ src/app/ventas/pages/carrito/carrito.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-carrito',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarritoComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
+
+  constructor() {
+    this.form = new FormGroup({
+      cantidad: new FormControl('', [Validators.required, Validators.min(1)]),
+    });
+  }
 
   ngOnInit(): void {
   }
