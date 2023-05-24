ca3465299ec757bd9c00e842b59707f5613d3eab
henryalejandro1229 <101129308+henryalejandro1229@users.noreply.github.com>
2023-05-23 21:09:18 -0600

    Fix - nombres empresa

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const host = '127.0.0.1';
 export const environment = {
   production: true,
-  url: `http://sastrerialospajaritos.proyectowebuni.com/api`,
-  urlImg: `http://sastrerialospajaritos.proyectowebuni.com/api/products/imageProducts/`,
+  url: `http://sportstore.proyectowebuni.com/api`,
+  urlImg: `http://sportstore.proyectowebuni.com/api/products/imageProducts/`,
   urlEmail: `https://appsportstoreserveremail-production.up.railway.app/app_sportstore_backend`,
 };
